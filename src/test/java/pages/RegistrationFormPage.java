df33cdca87bda6434d6377c94f2ade73d6c8bc38
PutintsevRoman <putintsevromann@yandex.ru>
2022-06-08 09:46:00 +0300

    added fix

--- src/test/java/pages/RegistrationFormPage.java
+++ src/test/java/pages/RegistrationFormPage.java
@@ -5,7 +5,7 @@ import pages.components.CalendarComponent;
 import pages.components.ResultTableComponents;
 
 import java.io.File;
-import java.util.Calendar;
+
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -14,17 +14,17 @@ public class RegistrationFormPage {
     CalendarComponent calendar = new CalendarComponent();
     ResultTableComponents resultTableComponents = new ResultTableComponents();
 
-    SelenideElement firstNameInput = $("#firstName")
-    ,lastNameInput =$("[id= lastName]")
-    ,userEmailInput = $("[id= userEmail]")
-    ,userNumberInput =$("[id=userNumber]")
-    ,subjectsInput =$("#subjectsInput")
-    ,currentAddressInput =$("[id=currentAddress]")
-    ,FileInput =$("[id=uploadPicture]")
-    ,stateInput = $("[id=react-select-3-input]")
-    ,cityInput =$("[id=react-select-4-input]")
-    ,dateInput =$("[id=dateOfBirthInput]")
-    ,buttom =$("#submit");
+    SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("[id= lastName]"),
+            userEmailInput = $("[id= userEmail]"),
+            userNumberInput = $("[id=userNumber]"),
+            subjectsInput = $("#subjectsInput"),
+            currentAddressInput = $("[id=currentAddress]"),
+            fileInput = $("[id=uploadPicture]"),
+            stateInput = $("[id=react-select-3-input]"),
+            cityInput = $("[id=react-select-4-input]"),
+            dateInput = $("[id=dateOfBirthInput]"),
+            buttom = $("#submit");
 
     public RegistrationFormPage openPage() {
         open("/automation-practice-form");
@@ -35,69 +35,77 @@ public class RegistrationFormPage {
         return this;
     }
 
-    public RegistrationFormPage SetfirstName() {
+    public RegistrationFormPage setFirstName() {
         firstNameInput.setValue("Alexey");
         return this;
     }
-    public RegistrationFormPage SetlastName() {
+
+    public RegistrationFormPage setLastName() {
         lastNameInput.setValue("Victorov");
         return this;
     }
-    public RegistrationFormPage SetuserEmail() {
+
+    public RegistrationFormPage setUserEmail() {
         userEmailInput.setValue("alex_vicktory@mail.com");
         return this;
     }
-    public RegistrationFormPage SetgenderRadio(int num) {
-        SelenideElement gender_radioInput =$("[for='gender-radio-"+ num +"']");
+
+    public RegistrationFormPage setGenderRadio(int num) {
+        SelenideElement gender_radioInput = $("[for='gender-radio-" + num + "']");
         gender_radioInput.click();
         return this;
     }
 
-    public RegistrationFormPage SetuserNumber () {
+    public RegistrationFormPage setUserNumber() {
         userNumberInput.setValue("1234567890");
         return this;
     }
 
-    public RegistrationFormPage Setcheckbox (int num) {
-        SelenideElement checkboxInput =$("[for='hobbies-checkbox-"+ num +"']");
+    public RegistrationFormPage setCheckbox(int num) {
+        SelenideElement checkboxInput = $("[for='hobbies-checkbox-" + num + "']");
         checkboxInput.click();
         return this;
     }
 
-    public RegistrationFormPage Setsubject () {
+    public RegistrationFormPage setSubject() {
         subjectsInput.setValue("H").pressEnter();
         return this;
     }
 
-    public RegistrationFormPage SetcurrentAddress () {
+    public RegistrationFormPage setCurrentAddress() {
         currentAddressInput.setValue("Some address");
         return this;
     }
 
-    public RegistrationFormPage Setfile () {
-        FileInput.uploadFile(new
+    public RegistrationFormPage setFile() {
+        fileInput.uploadFile(new
                 File("src\\test\\resources\\a.png"));
         return this;
-        }
-    public RegistrationFormPage SetCity () {
+    }
+
+    public RegistrationFormPage setCity() {
         cityInput.setValue("Noida").pressEnter();
         return this;
     }
-    public RegistrationFormPage SetState () {
+
+    public RegistrationFormPage setState() {
         stateInput.setValue("NCR").pressEnter();
         return this;
     }
-    public RegistrationFormPage SetDate (String month, String year,String day) {
+
+    public RegistrationFormPage setDate(String month, String year, String day) {
         dateInput.click();
         calendar.setDate(month, year, day);
         return this;
     }
-    public RegistrationFormPage Submit () {
+
+    public RegistrationFormPage submit() {
         buttom.click();
         return this;
     }
+
     public RegistrationFormPage checkResult(String key, String value) {
-        resultTableComponents.checkResult(key,value);
+        resultTableComponents.checkResult(key, value);
         return this;
     }
 }
--- src/test/java/test/TestForm.java
+++ src/test/java/test/TestForm.java
@@ -7,39 +7,38 @@ import pages.RegistrationFormPage;
 public class TestForm extends TestBase {
 
 
-
     @Test
-    void firstTest(){
+    void firstTest() {
 
         RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
         registrationFormPage.openPage()
-                .SetfirstName()
-                .SetlastName()
-                .SetuserEmail()
-                .SetgenderRadio(1)
-                .SetuserNumber()
-                .Setcheckbox(1)
-                .Setsubject()
-                .SetcurrentAddress()
-                .Setfile()
-                .SetState()
-                .SetCity()
-                .SetDate("August","1990","21")
-                .Submit();
+                .setFirstName()
+                .setLastName()
+                .setUserEmail()
+                .setGenderRadio(1)
+                .setUserNumber()
+                .setCheckbox(1)
+                .setSubject()
+                .setCurrentAddress()
+                .setFile()
+                .setState()
+                .setCity()
+                .setDate("August", "1990", "21")
+                .submit();
 
 
         //Checks
-        registrationFormPage.checkResult("Student Name","Alexey Victorov")
-                        .checkResult("Student Email","alex_vicktory@mail.com")
-                        .checkResult("Gender","Male")
-                        .checkResult("Mobile","1234567890")
-                        .checkResult("Date of Birth","21 August,1990")
-                        .checkResult("Subjects","Hindi")
-                        .checkResult("Hobbies","Sports")
-                        .checkResult("Picture","a.png")
-                        .checkResult("Address","Some address")
-                        .checkResult("State and City","NCR Noida");
+        registrationFormPage.checkResult("Student Name", "Alexey Victorov")
+                .checkResult("Student Email", "alex_vicktory@mail.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "1234567890")
+                .checkResult("Date of Birth", "21 August,1990")
+                .checkResult("Subjects", "Hindi")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "a.png")
+                .checkResult("Address", "Some address")
+                .checkResult("State and City", "NCR Noida");
 
     }
 
